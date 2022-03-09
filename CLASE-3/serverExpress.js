d6596a2bfe8cff81b170b93b6831e22555250161
FlorrMammana <floramammana1994@gmail.com>
2022-03-09 01:31:15 -0300

    cambios desafio 3

--- CLASE-3/serverExpress.js
+++ CLASE-3/serverExpress.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const data = require("./productos.json")
 const app = express();
 
+//puerto
 const PORT = 8080
-const server = app.listen(PORT, () => {
+
+app.listen(PORT, () => {
  console.log(`Servidor http escuchando en el puerto http://localhost:8080`)
+ console.log(`http://localhost:8080/productos`)
+ console.log(`http://localhost:8080/productoRandom`)
 })
-
+//Todos los productos
 app.get("/productos", (req, res) => {
-  res.json('productos.json');
+  res.json(data);
 });
 
+//producto random
 app.get("/productoRandom", (req, res) => {
-  res.json('productos.json');
+
+  res.json(data[Math.floor(Math.random()*data.length)]);
 });
-//server.on("error", error => console.log(`Error en servidor ${error}`))
+
+server.on("error", error => console.log(`Error en servidor ${error}`))
